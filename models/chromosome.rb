7a24b31ff33a1ad566813778f8e363fc08ee7afd
Jan Aerts <jan.aerts@gmail.com>
2009-03-11 12:01:17 +0000

    Added drawing code for chromosomes and readpairs

--- models/chromosome.rb
+++ models/chromosome.rb
@@ -1,8 +1,35 @@
 class Chromosome
-  attr_accessor :name, :length, :centromere_start, :centromere_stop
+  attr_accessor :name, :length, :centromere
   attr_accessor :readpairs
+  attr_accessor :normalized_length, :normalized_centromere #to 360
+  attr_accessor :bp_offset, :degree_offset
 
-  def initialize(name, length, centr_start, centr_stop)
-    @name, @length, @centromere_start, @centromere_stop = name, length, centr_start, centr_stop
+  def initialize(name, length, centr)
+    @name, @length, @centromere = name, length, centr
+    @normalized_length = (@length.to_f/GENOME_SIZE)*360
+    @normalized_centromere = (@centromere.to_f/GENOME_SIZE)*360
+    if @name == '1'
+      @bp_offset, @degree_offset = 0, 0
+    else
+      prev_chr = S.chromosomes[(@name.to_i - 1).to_s]
+      @bp_offset = prev_chr.bp_offset + prev_chr.length
+      @degree_offset = prev_chr.degree_offset + prev_chr.normalized_length
+    end
+  end
+
+  def draw(b, offset = 0)
+    b.no_fill
+    b.stroke_weight 3
+    if @name.to_i % 2 == 0
+      b.stroke 0
+    else
+      b.stroke 150
+    end
+    S.pline(@degree_offset, @degree_offset + @normalized_length, S.diameter, 0, 0, :buffer => b)
+
+    b.fill 0
+    b.no_stroke
+    b.ellipse(S.cx(@normalized_centromere + @degree_offset, S.radius), S.cy(@normalized_centromere + @degree_offset, S.radius),5,5)
+    b.text(@name, S.cx(@normalized_centromere + @degree_offset, S.radius + 15), S.cy(@normalized_centromere + @degree_offset, S.radius + 15))
   end
 end
--- models/readpair.rb
+++ models/readpair.rb
@@ -1,10 +1,35 @@
 class ReadPair
-  attr_accessor :reads
-  attr_accessor :code
+  attr_accessor :from_chr, :from_pos, :to_chr, :to_pos
+  attr_accessor :code, :qual
+  attr_accessor :normalized_bp_from, :normalized_bp_to
+  attr_accessor :normalized_degree_from, :normalized_degree_to
+  attr_accessor :interchromosomal
   
-  def initialize(from_chr, from_pos, to_chr, to_pos, code)
-    @reads = Array.new
-    @reads.push(Read.new(from_chr, from_pos, self))
-    @reads.push(Read.new(to_chr, to_pos, self))
+  def initialize(from_chr, from_pos, to_chr, to_pos, code, qual)
+    @from_chr = S.chromosomes[from_chr]
+    @to_chr = S.chromosomes[to_chr]
+    @from_pos, @to_pos, @code, @qual = from_pos.to_i, to_pos.to_i, code, qual.to_i
+
+    @interchromosomal = ( from_chr == to_chr ) ? false : true
+
+    @normalized_bp_from = @from_chr.bp_offset + @from_pos
+    @normalized_bp_to = @to_chr.bp_offset + @to_pos
+    @normalized_degree_from = (@normalized_bp_from.to_f/GENOME_SIZE)*360
+    @normalized_degree_to = (@normalized_bp_to.to_f/GENOME_SIZE)*360
+  end
+
+  def draw(b)
+    b.no_fill
+    b.stroke 200
+    b.stroke_weight 0.5
+    distance_from_circle = ( @interchromosomal ) ? 80 : 20
+    b.bezier(S.cx(@normalized_degree_from, S.radius - distance_from_circle),
+             S.cy(@normalized_degree_from, S.radius - distance_from_circle),
+             S.cx(@normalized_degree_from, S.radius - distance_from_circle - 50),
+             S.cy(@normalized_degree_from, S.radius - distance_from_circle - 50),
+             S.cx(@normalized_degree_to, S.radius - distance_from_circle - 50),
+             S.cy(@normalized_degree_to, S.radius - distance_from_circle - 50),
+             S.cx(@normalized_degree_to, S.radius - distance_from_circle),
+             S.cy(@normalized_degree_to, S.radius - distance_from_circle))
   end
 end
