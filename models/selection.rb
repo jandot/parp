ae6f7b60ef5ed0d44982bacefa55cb38d8d70331
Jan Aerts <jan.aerts@gmail.com>
2009-03-22 16:30:14 +0000

    Small changes to Selection

--- models/selection.rb
+++ models/selection.rb
@@ -2,24 +2,24 @@ class Selection
   attr_accessor :start_overview_degree, :stop_overview_degree
   attr_accessor :degree_offset, :bp_offset
   attr_accessor :chr, :start_pos, :stop_pos
-  attr_accessor :length
+  attr_accessor :length_bp
   attr_accessor :slice
   attr_accessor :label
 
   def initialize(start_degree, stop_degree, chr, start_pos, stop_pos, label)
     @start_overview_degree, @stop_overview_degree = start_degree, stop_degree
     @chr, @start_pos, @stop_pos = chr, start_pos, stop_pos
-    @length = ((@stop_overview_degree - @start_overview_degree).to_f/360)*GENOME_SIZE.to_i
+    @length_bp = ((@stop_overview_degree - @start_overview_degree).to_f/360)*GENOME_SIZE.to_i
     @label = label
 
-    offset = S.displays[:detail].bp_length
+    offset = S.displays[:detail].length_bp
     @slice = Slice.new(@chr, @start_pos, @stop_pos, S.displays[:detail])
     @slice.bp_offset = offset
     @slice.label = @label
   end
   
 #  def slice(display)
-#    offset = display.bp_length
+#    offset = display.length_bp
 #    slice = Slice.new(@chr, @start_pos, @stop_pos, display)
 #    slice.label = self.name
 #    slice.bp_offset = offset
